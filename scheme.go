360e5f2ce9b1642cd6f3c95a1fc05ca8af810fd4
Nelo-T. Wallus <red.brush9525@fastmail.com>
2026-09-03 21:39:22 +0200

    Add corev1alpha1
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

--- scheme.go
+++ scheme.go
@@ -1,15 +1,16 @@
 package kcp
 
 import (
+	corev1alpha1 "github.com/kcp-dev/sdk/apis/core/v1alpha1"
 	tenancyv1alpha1 "github.com/kcp-dev/sdk/apis/tenancy/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 )
 
-// Small helper only used internally to constructo ctrl-runtime clients
-// with the kcp tenancy API in the scheme to create workspaces.
+// Small helper only used internally.
 func tenancyScheme() *runtime.Scheme {
 	s := runtime.NewScheme()
 	utilruntime.Must(tenancyv1alpha1.AddToScheme(s))
+	utilruntime.Must(corev1alpha1.AddToScheme(s))
 	return s
 }
