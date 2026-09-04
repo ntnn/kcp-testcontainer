1c2c2846ed584d9c12a9a1d9366b409ee7096804
Nelo-T. Wallus <red.brush9525@fastmail.com>
2026-09-04 08:21:16 +0200

    Add notes on per-process and envtest.Sharded
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

--- once.go
+++ once.go
@@ -14,6 +14,8 @@ var (
 )
 
 // SingleOnce is like [Single] but always returns the same [SingleInstance].
+//
+// This is once per process. To deploy a single instance overall use [testcontainers.WithReuseByName].
 func SingleOnce(ctx context.Context, img string, opts ...testcontainers.ContainerCustomizer) (*SingleInstance, error) {
 	singleOnce.Do(func() {
 		singleOnceInst, errSingleOnce = Single(ctx, img, opts...)
@@ -28,6 +30,13 @@ var (
 )
 
 // ShardedOnce is like [Sharded] but always returns the same [ShardedInstance].
+//
+// This is once per process. Deploying across multiple processes is not
+// recommended since the kcp instance relies on pki, which for
+// kcp-testcontainer is held in memory.
+//
+// For a persistent sharded test setup use envtest.Sharded from kcp's multicluster-provider:
+// https://pkg.go.dev/github.com/kcp-dev/multicluster-provider@main/envtest#Sharded
 func ShardedOnce(ctx context.Context, img string, opts ...ShardedOption) (*ShardedInstance, error) {
 	shardedOnce.Do(func() {
 		shardedOnceInst, errShardedOnce = Sharded(ctx, img, opts...)
